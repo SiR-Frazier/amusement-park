67609cf5b44252ac00687640febc0ce46af519a6
bryce frazier and russell hofvendahl <russell@>
2018-01-11 13:15:07 -0800

    add images paragraph content

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
-  var age = parseInt(prompt("Welcome to Wonderland! How old are you?"));
-    console.log(age);
-    if (age >= 18) {
+  var height = parseInt(prompt("Welcome to Wonderland! Are you tall enough for the rides? Please answer in feet."));
+    if (height >= 5) {
       alert("Enjoy the rides!");
       $("#rides").show();
     } else {
-      alert("Sorry, kiddo!");
+      alert("Sorry! You must be at least 5 feet tall for all rides.");
     };
 });
